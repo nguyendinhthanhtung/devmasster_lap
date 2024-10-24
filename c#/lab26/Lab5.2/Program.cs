156ecb677928bb709144b8061c1f80cf2e77785a
nguyendinhthanhtung <nguyendinhthanhtung2k3@gmail.com>
2024-10-24 20:17:02 +0700

    full

---
+++ c#/lab26/Lab5.2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab5._2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// chương trình minh hoạ một số thao tác trên mảng 2 chiều 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //khai báo mảng 
+            int[,] a =
+            {
+                {4,6,9 },
+                {2,4,5 },
+                {9,2,6},
+                {1,6,3}
+
+            };
+            //duyệt mảng in theo hàng cột 
+            Console.WriteLine("noi dung mang: ");
+            for (int i = 0; i < a.GetUpperBound(0); i++) ;
+            {
+                Console.WriteLine();
+                    for (int j=0; j< a.GetUpperBound(1);j++)
+                {
+                    Console.WriteLine("{0},a[i,j]");
+                }
+                    Console.WriteLine();
+            }
+            //tìm phần tử có chỉ số hàng bàng số cột 
+            Console.WriteLine("cac phan tu co chi so hang bang so cot:");
+            for(int i = 0;i < a.GetUpperBound(0); i++) 
+            {
+                for (int j = 0; j <= a.GetUpperBound(1); j++)
+                {
+                    if (i == j)
+                        Console.WriteLine("{0}", a[i, j]);
+
+                }
+                   
+            }
+            //cac phan tu lon nhat tren hang
+            Console.WriteLine("cac phan tu lon nhat tren hang");
+            for (int i = 0; i <= a.GetUpperBound(0); i++)
+            {
+                int max = a[i, 0];
+                for (int j = 0;j <= a.GetUpperBound(1); j++)
+                {
+                    if (max<a[i, j])
+                        max = a[i, j];
+                }
+                Console.WriteLine("hang {0}:{1}");
+
+            }
+
+
+        }
+    }
+}
---
+++ c#/lab26/lab26/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace lab26
+{
+    internal class Program
+    {
+        /// <summary>
+        /// một số thao tác với mảng 1 chiều 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //khai báo và khởi tạo mảng 1 chiều 
+            int[] m = { 5, 8, 3, 0, 2, 1, 7, 8 };
+            //duyệt mnagr và in ra dữ liệu 
+            Console.WriteLine("cac phan tu cua mang ");
+            for (int i = 0; i < m.Length; i++)
+            {
+                Console.WriteLine("{0},m[i]");
+            }
+            //tìm phần tử lớn nhất 
+            int max =m[0];
+            for (int i = 0; i < m.Length; i++) 
+            {
+                if (max < m[i])
+                    max = m[i];
+            }
+            Console.WriteLine("\n phan tu lon nhat: " + max);
+            //kiểm tra mảng có đối xứng không 
+            bool kt = true;
+            for (int i = 0; i < m.Length / 2; i++)
+            {
+                if (m[i] != m[m.Length - 1 - i])
+                {
+                    kt = false;
+                    break;
+                }
+            }
+            if (kt)
+                Console.WriteLine("mang doi xung ");
+            else
+                Console.WriteLine("mang khong doi xung");
+        }
+    }
+}
